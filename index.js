96aec496158b2431ecc31449f1e970a41689560d
tamzid74 <tamzidal74@gmail.com>
2023-08-29 18:59:34 +0600

    start stop and reset button added

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const hours = document.getElementById('hours-field');
 const minutes = document.getElementById('minutes-field');
 const seconds = document.getElementById('seconds-field');
+let interval;
 
-let num =0
-const interval = setInterval(()=>{
-    const sec = ++num;
-    seconds.innerText = sec;
-    if(sec >= 60){
-        clearInterval(interval);
-    }
-},1000)
+function startBtn(){
+    let num =0
+     interval = setInterval(()=>{
+        const sec = ++num;
+        seconds.innerText = sec;
+        if(sec >= 60){
+            clearInterval(interval);
+        }
+    },1000)
+}
+
+function stopBtn(){
+            clearInterval(interval);
+}
+function resetBtn(){
+    seconds.innerText = '00';
+}
